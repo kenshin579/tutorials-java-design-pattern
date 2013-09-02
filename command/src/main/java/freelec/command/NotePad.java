b3df5d17ec8f4cb462de14263dcc20bc2154d8e7
kenshin579 <kenshin579@hotmail.com>
2013-09-02 19:06:45 +0900

    - notepad 수정

--- command/src/main/java/freelec/command/NotePad.java
+++ command/src/main/java/freelec/command/NotePad.java
@@ -1,76 +1,76 @@
-package freelec.command;
+package freelec.command
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.*
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
 
-public class NotePad implements ActionListener {
+public class NotePad implements ActionListener 
 
-    private Menu file;
-    private MenuItem _new;
-    private MenuItem open;
-    private MenuItem save;
-    private MenuItem saveas;
-    private MenuItem exit;
+    private Menu file
+    private MenuItem _new
+    private MenuItem open
+    private MenuItem save
+    private MenuItem saveas
+    private MenuItem exit
 
-    private MenuBar bar;
+    private MenuBar bar
 
-    private TextArea text;
-    private Frame frame;
+    private TextArea text
+    private Frame frame
 
-    public NotePad() {
-        text = new TextArea();
-        frame = new Frame("NotePad");
-        file = new Menu("File");
+    public NotePad() 
+        text = new TextArea()
+        frame = new Frame("NotePad")
+        file = new Menu("File")
 
-        _new = new MenuItem("New");
-        open = new MenuItem("Open");
-        save = new MenuItem("Save");
-        saveas = new MenuItem("Save as");
-        exit = new MenuItem("Exit");
-        bar = new MenuBar();
+        _new = new NewMenuItem("New", text)
+        open = new OpenMenuItem("Open", frame)
+        save = new SaveMenuItem("Save", frame)
+        saveas = new SaveAsMenuItem("Save as", frame)
+        exit = new ExitMenuItem("Exit")
+        bar = new MenuBar()
 
-        file.add(_new);
-        file.add(open);
-        file.add(save);
-        file.add(saveas);
-        file.add(exit);
-        bar.add(file);
+        file.add(_new)
+        file.add(open)
+        file.add(save)
+        file.add(saveas)
+        file.add(exit)
+        bar.add(file)
 
 
-        frame.addWindowListener(
-                new WindowAdapter() {
-                    public void windowClosing(WindowEvent e) {
-                        System.exit(0);
-                    }
-                }
-        );
+        frame.addWindowListener
+                new WindowAdapter() 
+                    public void windowClosing(WindowEvent e) 
+                        System.exit(0)
+                    
+                
+        )
 
-        frame.setMenuBar(bar);
-        frame.add(text);
-        frame.setBounds((1280 - 200) / 2, (1024 - 150) / 2, 200, 150);
-        frame.setVisible(true);
+        frame.setMenuBar(bar)
+        frame.add(text)
+        frame.setBounds((1280 - 200) / 2, (1024 - 150) / 2, 200, 150)
+        frame.setVisible(true)
 
-        addListener();
-    }
+        addListener()
+    
 
-    public void addListener() {
-        _new.addActionListener(this);
-        open.addActionListener(this);
-        save.addActionListener(this);
-        saveas.addActionListener(this);
-        exit.addActionListener(this);
-    }
+    public void addListener() 
+        _new.addActionListener(this)
+        open.addActionListener(this)
+        save.addActionListener(this)
+        saveas.addActionListener(this)
+        exit.addActionListener(this)
+    
 
-    public void actionPerformed(ActionEvent e) {
-        Command com = (Command) e.getSource();
-        com.execute();
-    }
+    public void actionPerformed(ActionEvent e) 
+        Command com = (Command) e.getSource()
+        com.execute()
+    
 
-    public static void main(String args[]) {
-        new NotePad();
-    }
+    public static void main(String args[]) 
+        new NotePad()
+    
 
-}	
+}
