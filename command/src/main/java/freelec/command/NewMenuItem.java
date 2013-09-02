ab209d887cb7530a3200a586601db6300b98009a
kenshin579 <kenshin579@hotmail.com>
2013-09-02 19:26:23 +0900

    코멘트 추가

--- command/src/main/java/freelec/command/NewMenuItem.java
+++ command/src/main/java/freelec/command/NewMenuItem.java
@@ -6,6 +6,10 @@ public class NewMenuItem extends MenuItem implements Command {
 
     private TextArea text;
 
+    /*
+    NewMenuItem 클래스의 객체는 TextArea 클래스 객체와 연관되어 있음.
+    중개자가 없으므로 연관된 클래스의 변화가 발생하면 관계된 크래스의 변화는 필수가 되는 이슈가 있음.
+     */
     public NewMenuItem(String s, TextArea text) {
         super(s);
         this.text = text;
--- command/src/main/java/freelec/command/NotePad.java
+++ command/src/main/java/freelec/command/NotePad.java
@@ -1,76 +1,81 @@
-package freelec.command
+package freelec.command;
 
-import java.awt.*
-import java.awt.event.ActionEvent
-import java.awt.event.ActionListener
-import java.awt.event.WindowAdapter
-import java.awt.event.WindowEvent
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class NotePad implements ActionListener 
+/**
+ * Command 인터페이스를 두어서 조건절을 사용하지 않고도 같은 인터페이스로 각각 다른 기능을 수행할 수 있다.
+ */
+public class NotePad implements ActionListener {
 
-    private Menu file
-    private MenuItem _new
-    private MenuItem open
-    private MenuItem save
-    private MenuItem saveas
-    private MenuItem exit
+    private Menu file;
+    private MenuItem _new;
+    private MenuItem open;
+    private MenuItem save;
+    private MenuItem saveas;
+    private MenuItem exit;
 
-    private MenuBar bar
+    private MenuBar bar;
 
-    private TextArea text
-    private Frame frame
+    private TextArea text;
+    private Frame frame;
 
-    public NotePad() 
-        text = new TextArea()
-        frame = new Frame("NotePad")
-        file = new Menu("File")
+    public NotePad() {
+        text = new TextArea();
+        frame = new Frame("NotePad");
+        file = new Menu("File");
 
-        _new = new NewMenuItem("New", text)
-        open = new OpenMenuItem("Open", frame)
-        save = new SaveMenuItem("Save", frame)
-        saveas = new SaveAsMenuItem("Save as", frame)
-        exit = new ExitMenuItem("Exit")
-        bar = new MenuBar()
+        _new = new NewMenuItem("New", text);
+        open = new OpenMenuItem("Open", frame);
+        save = new SaveMenuItem("Save", frame);
+        saveas = new SaveAsMenuItem("Save as", frame);
+        exit = new ExitMenuItem("Exit");
+        bar = new MenuBar();
 
-        file.add(_new)
-        file.add(open)
-        file.add(save)
-        file.add(saveas)
-        file.add(exit)
-        bar.add(file)
+        file.add(_new);
+        file.add(open);
+        file.add(save);
+        file.add(saveas);
+        file.add(exit);
+        bar.add(file);
 
 
-        frame.addWindowListener
-                new WindowAdapter() 
-                    public void windowClosing(WindowEvent e) 
-                        System.exit(0)
-                    
-                
-        )
+        frame.addWindowListener(
+                new WindowAdapter() {
+                    public void windowClosing(WindowEvent e) {
+                        System.exit(0);
+                    }
+                }
+        );
 
-        frame.setMenuBar(bar)
-        frame.add(text)
-        frame.setBounds((1280 - 200) / 2, (1024 - 150) / 2, 200, 150)
-        frame.setVisible(true)
+        frame.setMenuBar(bar);
+        frame.add(text);
+        frame.setBounds((1280 - 200) / 2, (1024 - 150) / 2, 200, 150);
+        frame.setVisible(true);
 
-        addListener()
-    
+        addListener();
+    }
 
-    public void addListener() 
-        _new.addActionListener(this)
-        open.addActionListener(this)
-        save.addActionListener(this)
-        saveas.addActionListener(this)
-        exit.addActionListener(this)
-    
+    public void addListener() {
+        _new.addActionListener(this);
+        open.addActionListener(this);
+        save.addActionListener(this);
+        saveas.addActionListener(this);
+        exit.addActionListener(this);
+    }
 
-    public void actionPerformed(ActionEvent e) 
-        Command com = (Command) e.getSource()
-        com.execute()
-    
+    public void actionPerformed(ActionEvent e) {
+        // 이벤트 객체는 모두 Command 인터페이스를 상속받은 것들이므로 로 형 변환 (Class Type Casting)될 수 있다.
+        Command com = (Command) e.getSource();
+        // Command 인터페이스를 구현한 객체는 execute() 메서드를 구현해야 한다.
+        com.execute();
+    }
 
-    public static void main(String args[]) 
-        new NotePad()
-    
+    public static void main(String args[]) {
+        new NotePad();
+    }
 
-}
+}	
