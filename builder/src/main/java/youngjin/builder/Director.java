130148bac574caef0916b57773cfcd1318f94d90
kenshin579 <kenshin579@hotmail.com>
2013-10-07 19:36:30 +0900

    comment 수정

--- builder/src/main/java/youngjin/builder/Director.java
+++ builder/src/main/java/youngjin/builder/Director.java
@@ -4,13 +4,13 @@ public class Director {
     private Builder builder;
 
     public Director(Builder builder) {        // Builder의 Subclass의 인스턴스가 주어지므로
-        this.builder = builder;             // builder 필드에 저장해 둔다.
+        this.builder = builder;               // builder 필드에 저장해 둔다.
     }
 
     public void construct() {                    // 문서구축
-        builder.makeTitle("Greeting");              // 타이틀
-        builder.makeString("아침과 낮에");         // 문자열
-        builder.makeItems(new String[]{            // 개별항목
+        builder.makeTitle("Greeting");           // 타이틀
+        builder.makeString("아침과 낮에");          // 문자열
+        builder.makeItems(new String[]{          // 개별항목
                 "좋은 아침입니다",
                 "안녕하세요",
         });
