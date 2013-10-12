03cb5ae61fcf29587ea7c4ec5bf008e35e766cc3
kenshin579 <kenshin579@hotmail.com>
2013-10-12 13:15:25 +0900

    proxy 패턴 추가

---
+++ proxy/src/main/java/youngjin/proxy/Printer.java
@@ -0,0 +1,39 @@
+package youngjin.proxy;
+
+public class Printer implements Printable {
+    private String name;
+
+    public Printer() {
+        heavyJob("Printer의 인스턴스를 생성 중");
+    }
+
+    public Printer(String name) {                   // 생성자
+        this.name = name;
+        heavyJob("Printer의 인스턴스 (" + name + ")을 생성 중");
+    }
+
+    public void setPrinterName(String name) {       // 이름의 설정
+        this.name = name;
+    }
+
+    public String getPrinterName() {                // 이름의 취득
+        return name;
+    }
+
+    public void print(String string) {              // 이름을 붙여 표시
+        System.out.println("=== " + name + " ===");
+        System.out.println(string);
+    }
+
+    private void heavyJob(String msg) {             // 무거운 일(의 예정)
+        System.out.print(msg);
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            System.out.print(".");
+        }
+        System.out.println("완료");
+    }
+}
---
+++ proxy/src/main/java/youngjin/proxy/PrinterProxy.java
@@ -0,0 +1,35 @@
+package youngjin.proxy;
+
+public class PrinterProxy implements Printable {
+    private String name;                    // 이름
+    private Printer real;                   // 「본인」
+
+    public PrinterProxy() {
+    }
+
+    public PrinterProxy(String name) {      // 생성자
+        this.name = name;
+    }
+
+    public synchronized void setPrinterName(String name) {  // 이름의 설정
+        if (real != null) {
+            real.setPrinterName(name);      // 「본인」에게도 설정한다
+        }
+        this.name = name;
+    }
+
+    public String getPrinterName() {        // 이름의 설정
+        return name;
+    }
+
+    public void print(String string) {     // 표시
+        realize();
+        real.print(string);
+    }
+
+    private synchronized void realize() {   // 「본인」을 생성
+        if (real == null) {
+            real = new Printer(name);
+        }
+    }
+}
