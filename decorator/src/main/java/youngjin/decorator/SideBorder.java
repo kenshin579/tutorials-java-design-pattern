630f65a72b3194cb0cb65be0b5f8400c84843fbf
kenshin579 <kenshin579@hotmail.com>
2013-10-12 15:12:09 +0900

    decorator 패턴 추가

---
+++ decorator/src/main/java/youngjin/decorator/SideBorder.java
@@ -0,0 +1,22 @@
+package youngjin.decorator;
+
+public class SideBorder extends Border {
+    private char borderChar;
+
+    public SideBorder(Display display, char ch) {
+        super(display);
+        this.borderChar = ch;
+    }
+
+    public int getColumns() {
+        return 1 + display.getColumns() + 1;
+    }
+
+    public int getRows() {
+        return display.getRows();
+    }
+
+    public String getRowText(int row) {
+        return borderChar + display.getRowText(row) + borderChar;
+    }
+}
---
+++ decorator/src/main/java/youngjin/decorator/StringDisplay.java
@@ -0,0 +1,25 @@
+package youngjin.decorator;
+
+public class StringDisplay extends Display {
+    private String string;
+
+    public StringDisplay(String string) {
+        this.string = string;
+    }
+
+    public int getColumns() {
+        return string.getBytes().length;
+    }
+
+    public int getRows() {
+        return 1;
+    }
+
+    public String getRowText(int row) {
+        if (row == 0) {
+            return string;
+        } else {
+            return null;
+        }
+    }
+}
