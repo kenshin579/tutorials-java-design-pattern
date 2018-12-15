1dbebaf652dd041cb254c3485ebee6be7467315b
Frank <frankyoh@tmon.co.kr>
2018-12-15 21:08:54 +0900

    updated observer

---
+++ observer/src/main/java/freelec/noobserver/compile.sh
@@ -0,0 +1,8 @@
+set classpath .;mysql-connector-java-5.1.14-bin.jar
+javac *.java
+rmic StockImpl
+copy StockImpl_Stub.class ..\freelec.noobserver.client
+start rmiregistry
+start java JulyThread
+start java StockServer
+
